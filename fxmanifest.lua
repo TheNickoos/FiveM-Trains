0059842392e82274ea283d222536cfa9fee9e95d
Nickoos <maistriaux.geoffroy@gmail.com>
2020-08-08 14:46:33 +0200

    Initial project

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+client_scripts {
+	'config.lua',
+	'Locales/en.lua',
+	'Locales/es.lua',
+	'Locales/fr.lua',
+	'client/client.lua'
+}
+
+server_scripts {
+	'config.lua',
+	'server/server.lua'
+}
+
+this_is_a_map 'yes'
+
+fx_version 'adamant'
+game 'gta5'
+description 'FiveM Trains System'
